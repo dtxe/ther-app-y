0e7390f5d5319e531b00b0ff94192781833648ae
Simeon Wong <simeon@dtxe.net>
2015-02-28 15:21:05 -0500

    Add "Tesla" shaped motion trace. Add FFT filter and trace animation. FFT filter using 0.02 - 30Hz seems to work decently well for recovering position.

--- signals/antigravity2.m
+++ signals/antigravity2.m
@@ -3,13 +3,19 @@
 % Simeon Wong
 % 2014 Feb 28
 
+close all
+clear
 
-data = importdata('./therappy1424922398062.txt');
+data = importdata('./therappy1425147273141.txt');
+% data = importdata('./therappy1424922398062.txt');
+
+t_begin = tic;
+
 
 %% Data Preprocessing
 % Separate accelerometer and rotation
 % count number of a, r
-accl_idx = cellfun(@(c) c == 'a', data.textdata(:,2));
+accl_idx = cellfun(@(c) strcmp(c, 'a'), data.textdata(:,2));
 accl_len = sum(accl_idx);
 
 % get accl data
@@ -20,63 +26,151 @@ accl_data = data.data(accl_idx,:);
 accl_t = accl_t - accl_t(1);
 
 
-%% Filter
+% gyroscope data
+gyro_idx = cellfun(@(c) strcmp(c, 'r'), data.textdata(:,2));
+gyro_len = sum(gyro_idx);
+gyro_t = str2double(data.textdata(gyro_idx,1));
+gyro_data = data.data(gyro_idx,:);
 
 
-srate = 1000/mean(diff(accl_t));
 
-% Setup filter
-[z,p,k] = butter(4, [1 10]/(srate/2));
-btr_sos = zp2sos(z,p,k);
+%% Resample
+% Need to resample, since std-dev is +/- 20% of mean time difference
 
-% Filter data
-filtd = zeros(accl_len, 3);
-for kk = 1:3
-    filtd(:,kk) = filtfilt(btr_sos, 1, accl_data(:,kk));
+% create new time vector
+avg_accl_diff = mean(diff(accl_t));
+avg_accl_diff = round(avg_accl_diff)/4;
+
+accl_re_t = (0:avg_accl_diff:accl_t(end))';
+accl_re_data = interp1(accl_t, accl_data, accl_re_t);
+
+accl_re_srate = 1000/avg_accl_diff;
+accl_re_len = length(accl_re_data);
+
+% Plot resampled things
+if 1
+    pltitle = {'X-axis Raw Accl', 'Y-axis Raw Accl', 'Z-axis Raw Accl', 'X-axis Re Accl', 'Y-axis Re Accl', 'Z-axis Re Accl',};
+
+    figure;
+    for kk = 1:3
+        ax(kk) = subplot(2,3,kk);
+        plot(accl_t, accl_data(:,kk));
+        ylabel('Accel (ms^{-2})');
+        xlabel('Time (ms)');
+        title(pltitle{kk});
+
+        ax(kk+3) = subplot(2,3,kk+3);
+        plot(accl_re_t, accl_re_data(:,kk));
+        ylabel('Accel (ms^-2)');
+        xlabel('Time (ms)');
+        title(pltitle{kk+3});
+    end
+    
+    linkaxes(ax);
 end
 
-pltitle = {'X-axis Raw Accl', 'Y-axis Raw Accl', 'Z-axis Raw Accl', 'X-axis Filt Accl', 'Y-axis Filt Accl', 'Z-axis Filt Accl',};
+%% Filter
+
+FILTER_TYPE = 'fft';
+
+% filter selection
+if strcmp(FILTER_TYPE, 'iir')
+    % Setup filter
+    [z,p,k] = butter(4, [0.05 25]/(accl_re_srate/2), 'bandpass');
+    btr_sos = zp2sos(z,p,k);
+
+    % Filter data
+    accl_re_filtd = zeros(accl_re_len, 3);
+    for kk = 1:3
+        accl_re_filtd(:,kk) = sosfilt(btr_sos, accl_re_data(:,kk));
+    end
+    
+elseif strcmp(FILTER_TYPE, 'fft')
+    % figure out what to cut
+    fq = linspace(0, accl_re_srate/2, accl_re_len);
+    fq_gain = ones(accl_re_len, 1);
+    
+    fq_gain((fq < 0.02)) = 0;
+    fq_gain((fq > 30)) = 0;
+    
+    fq_gain(accl_re_len:-1:round(accl_re_len/2)+1) = fq_gain(1:round(accl_re_len/2));
+    
+    accl_re_filtd = zeros(accl_re_len, 3);
+    for kk = 1:3
+        accl_re_filtd(:,kk) = real(ifft(fq_gain .* fft(accl_re_data(:,kk))));
+    end
+end
+
+pltitle = {'X-axis Re Accl', 'Y-axis Re Accl', 'Z-axis Re Accl', 'X-axis Filt Accl', 'Y-axis Filt Accl', 'Z-axis Filt Accl',};
 
 figure;
 for kk = 1:3
     ax(kk) = subplot(2,3,kk);
-    plot(accl_t, accl_data(:,kk));
+    plot(accl_re_t, accl_re_data(:,kk));
     ylabel('Accel (ms^{-2})');
     xlabel('Time (ms)');
     title(pltitle{kk});
     
     ax(kk+3) = subplot(2,3,kk+3);
-    plot(accl_t, filtd(:,kk));
+    plot(accl_re_t, accl_re_filtd(:,kk));
     ylabel('Accel (ms^-2)');
     xlabel('Time (ms)');
     title(pltitle{kk+3});
 end
 
-accl_data = filtd;
+linkaxes(ax);
+
+% accl_data = filtd;
 
 
 %% Integration
-vel = zeros(accl_len, 3);
+
+% accl_re_filtd(:,1) = accl_re_filtd(:,1) - mean(accl_re_filtd(:,1));
+% accl_re_filtd(:,2) = accl_re_filtd(:,2) - mean(accl_re_filtd(:,2));
+% accl_re_filtd(:,3) = accl_re_filtd(:,3) - mean(accl_re_filtd(:,3));
+
+
+vel = zeros(accl_re_len, 3);
 for kk = 1:3
     % initial velocity is zero
-    vel(1,kk) = accl_data(1,kk)*(accl_t(2) - accl_t(1))/1000;
+    vel(1,kk) = accl_re_filtd(1,kk)*(avg_accl_diff/1000);
     
-    for jj = 2:accl_len
-        vel(jj,kk) = vel(jj-1,kk) + accl_data(jj,kk)*(accl_t(jj)-accl_t(jj-1))/1000;
+    for jj = 2:accl_re_len
+        vel(jj,kk) = vel(jj-1,kk) + accl_re_filtd(jj,kk)*(avg_accl_diff/1000);
     end
 end
 
-pos = zeros(accl_len, 3);
+pos = zeros(accl_re_len, 3);
 for kk = 1:3
-    pos(1,kk) = vel(1,kk)*(accl_t(2) - accl_t(1))/1000;
+    pos(1,kk) = vel(1,kk)*(avg_accl_diff/1000);
     
-    for jj = 2:accl_len
-        pos(jj,kk) = pos(jj-1,kk) + vel(jj,kk)*(accl_t(2) - accl_t(1))/1000;
+    for jj = 2:accl_re_len
+        pos(jj,kk) = pos(jj-1,kk) + vel(jj,kk)*(avg_accl_diff/1000);
     end
 end
 
 figure;
 plot3(pos(:,1), pos(:,2), pos(:,3));
+daspect([1 1 1]);
 
+%% Timing
+toc(t_begin);
 
+%% Animate
+vidwriter = VideoWriter(['test.avi']);
+open(vidwriter);
+f = figure(50);
+for tt = 1:20:length(pos)
+    hold on;
+    plot3(pos(:,1), pos(:,2), pos(:,3));
+    plot3(pos(1:tt,1), pos(1:tt,2), pos(1:tt,3), 'r', 'LineWidth', 3);
+    daspect([1 1 1]);
+    
+    refresh(f);
+    
+    writeVideo(vidwriter, getframe(f));
+    clf(f);
+end
+
+close(vidwriter);
 
