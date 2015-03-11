09bd629941c2e0f142fa9dd5cfc2da5390bea450
Simeon Wong <simeon@dtxe.net>
2015-03-11 16:29:39 -0400

    gifs work

--- presentation/generate_sine_gif.m
+++ presentation/generate_sine_gif.m
@@ -3,13 +3,13 @@
 % Simeon Wong
 % 2015 March 10
 
-NUM_WINDOWS  = 30;
-WND_LENGTH   = 100;
+NUM_WINDOWS  = 150;
+WND_LENGTH   = 250;
 NUM_SAMPLES  = 1000;
 
-t = linspace(0,2*pi, NUM_SAMPLES);
+t = linspace(0, 4*pi, NUM_SAMPLES);
 
-x1 = sin(50*t);
+x1 = sin(30*t);
 x2 = sin(2*t);
 x3 = sin(5*t);
 x4 = sin(7*t);
@@ -17,11 +17,23 @@ x4 = sin(7*t);
 xout = x1 .* x2 .* x3 .* x4;
 
 f = figure(50);
-set(f, 'Color', [0.176,0.02,0.294]);
+set(f, 'Color', [0.176,0.02,0.294], 'Position', [100 100 800 400]);
 plot(xout, 'Color', 'white', 'LineWidth', 3);
 axis off
 
 
 for kk = 1:NUM_WINDOWS
-    wnd_start = (kk-1)*(NUM_SAMPLES-WND_LENGTH);
+    wnd_start = (kk-1)*(NUM_SAMPLES/2)/NUM_WINDOWS;
+    xlim(wnd_start + [0, WND_LENGTH-1]);
+    ylim([-0.6 0.6]);
+    
+    frame = getframe(f);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,16);
+    
+    if kk == 1
+        imwrite(imind,cm,'loading.gif','gif', 'Loopcount',inf,'DelayTime',1/40);
+    else
+        imwrite(imind,cm,'loading.gif','gif', 'WriteMode','append','DelayTime',1/40);
+    end
 end
