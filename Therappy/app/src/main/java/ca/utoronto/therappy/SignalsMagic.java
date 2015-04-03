c3703c6f28dda670600e050c9edd6bbe8cd0c07d
Simeon Wong <simeon@dtxe.net>
2015-04-03 19:04:55 -0400

    move signals processing code into independent module.

--- Therappy/app/src/main/java/ca/utoronto/therappy/SignalsMagic.java
+++ Therappy/app/src/main/java/ca/utoronto/therappy/SignalsMagic.java
@@ -3,5 +3,78 @@ package ca.utoronto.therappy;
 /**
  * Created by simeon on 2015-03-14.
  */
+
 public class SignalsMagic {
+
+    public SignalsMagic () {
+        // pass the loaded acceleration and rotation data here
+
+    }
+
+    protected double[] doFilterNoDC_FFT(double[] datain, double hicutoff) {
+        // filter the signal using an FFT / iFFT algorithm, removing the DC component, and any
+        // components above the specified hicutoff
+        //      hicutoff should be provided as normalized frequency
+        //
+        // Essentially, this function will:
+        //   - transform the signal into frequency space (using FFT)
+        //   - create a vector of multiplication ratios
+        //   - zero out the multiplication ratio vectors that we want to filter out
+        //   - element-wise multiply the ratio vector with the frequency space of signal
+        //   - do inverse FFT to recover filtered signal
+
+        int num_samples = datain.length;
+
+        // find corresponding index
+        int hicutoffidx = (int) Math.ceil(num_samples * hicutoff);
+
+        // convert datain to complex format
+        Complex [] cpx_datain = new Complex[num_samples];
+        for(int kk = 0; kk < num_samples; kk++) {
+            cpx_datain[kk] = new Complex(datain[kk]);
+        }
+
+        // FFT MAGICKS HAPPENS HERE
+        // ***************
+        Complex [] fftoutput = FFT.fft(cpx_datain);
+        // ***************
+
+        // create a vector of things to zero out, set everything to 1
+        double[] zeroidx = new double[num_samples];
+        for (int kk = 0; kk < num_samples; kk++) {
+            zeroidx[kk] = 1;
+        }
+
+        // zero out DC component
+        zeroidx[0] = 0;
+
+        // zero out high frequency components above the hicutoff
+        int vecmiddle = (int) Math.ceil(num_samples/2);
+        for(int kk = hicutoffidx; kk < vecmiddle; kk++) {
+            zeroidx[kk] = 0;
+        }
+
+        // mirror the zero-out vector (since FFT is mirrored)
+        for(int kk = 0; kk < vecmiddle; kk++) {
+            zeroidx[num_samples - kk - 1] = zeroidx[kk];
+        }
+
+        // actually zero out the components that need to be zeroed out
+        for(int kk = 0; kk < num_samples; kk++) {
+            fftoutput[kk] = fftoutput[kk].times(zeroidx[kk]);
+        }
+
+        // INVERSE FFT MAGICKS HAPPENS HERE
+        // ***************
+        Complex[] cpx_output = FFT.ifft(fftoutput);
+        // ***************
+
+        // get real part of the FFT output.
+        double[] output = new double[num_samples];
+        for(int kk = 0; kk < num_samples; kk++) {
+            output[kk] = cpx_output[kk].re;
+        }
+
+        return output;
+    }
 }
