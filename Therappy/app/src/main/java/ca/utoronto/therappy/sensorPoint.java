1dbfde9131d25c6beab31c1ff9a8b3e5f63c4e0d
Simeon Wong <simeon@dtxe.net>
2015-04-03 19:10:34 -0400

    add data type parameter for sensorpoint.

--- Therappy/app/src/main/java/ca/utoronto/therappy/sensorPoint.java
+++ Therappy/app/src/main/java/ca/utoronto/therappy/sensorPoint.java
@@ -4,12 +4,17 @@ package ca.utoronto.therappy;
  * Created by Andrew on 26/03/2015.
  */
 public class sensorPoint implements Comparable<sensorPoint> {
+    public static final int DATA_ROTATIONVEC = 2;
+    public static final int DATA_ACCELERATION = 1;
+
     private final double time;
     private final double[] value;
+    private final int datatype;
 
-    public sensorPoint(double time, double[] val) {
+    public sensorPoint(double time, double[] val, int datatype) {
         this.time = time;
         this.value = val;
+        this.datatype = datatype;
     }
 
     public double getTime() {
@@ -20,6 +25,11 @@ public class sensorPoint implements Comparable<sensorPoint> {
         return this.value;
     }
 
+    public int getDataType() {
+        return this.datatype;
+    }
+
+    // allows for sorting of sensorPoint data using native libraries.
     @Override
     public int compareTo(sensorPoint p) {
         return (int) (this.time - p.time);
